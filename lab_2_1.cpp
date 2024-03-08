bc2452d107d1bf2bea50d0cff496cb40e4a1c151
stas <st.stanislove@yandex.ru>
2024-03-08 17:28:42 +0300

    small changes

--- lab_2_1.cpp
+++ lab_2_1.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
-int factorial(int number)
-{
-    if (number <= 1)
-    {
+int factorial(int number) {
+    if (number <= 1) {
         return 1;
     }
-    else
-    {
+    else {
         return  number * factorial(number - 1);
     }
 }
 
-int main ()
-{
+int main () {
     int n;
     std::cin >> n;
     std::cout << factorial(n) << std::endl;
